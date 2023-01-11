34d9a78b742d88c884f019ca83075e692fa2354e
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-01-11 13:58:32 +0600

    Update cycle_detect.cpp

--- DBMS/cycle_detect.cpp
+++ DBMS/cycle_detect.cpp
@@ -1,26 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-
-vector<ll>g[10];
+const ll N = 1e5+5;
+vector<ll>g[N];
 ll node, element, total_operation;
 map<char,vector<string>>mp;
 bool Cycle=false;
-bool vis[10];
+ll vis[N];
+set<char>st;
 
 void dfs(ll node)
 {
-    if(Cycle)return;
+    
     vis[node]=1;
     for(auto x: g[node])
     {   
-        if(!vis[x])dfs(x);
-        if(vis[node] && x!=node)
+        if(vis[x]==0)dfs(x);
+        if(vis[x]==1)
         {
             Cycle=true;
             return;
         }
+        if(vis[x]==2)continue;
     }
+    vis[node]=2;
 }
 
 int main()
@@ -31,20 +34,26 @@ int main()
     string s="";
     s+=c1;
     s+=c2;
-    mp[c3].push_back(s);}
+    mp[c3].push_back(s);
+    st.insert(c2);}
     for(auto x: mp){
         for(ll i=0;i<x.second.size();i++)
         {   
             bool flag=false;
             for(ll j=i+1;j<x.second.size();j++)
             {   
+                if((x.second[j][1]-'0')==(x.second[i][1]-'0'))
+                {
+                    flag=true;
+                }
+                if(!flag){
                 for(ll k=0;k<g[x.second[i][1]-'0'].size();k++)
                 {
                     if(g[x.second[i][1]-'0'][k]==x.second[j][1]-'0')
                     {
                         flag=true;break;
                     }
-                }
+                }}
                 if(!flag){
                 if(x.second[i][0]=='r')
                 {
@@ -62,15 +71,21 @@ int main()
                 }
             }}
         }
+        element++;
     }
-    dfs(2);
-    // for(ll i=1;i<=node;i++)
-    // {
-    //     if(!vis[i])
-    //     {
-    //         dfs(i);
-    //     }
-    // }
+    node=st.size();
+    for(ll i=1;i<=4;i++)
+    {   cout<<i<<": ";
+        for(ll j=0;j<g[i].size();j++)cout<<g[i][j]<<" ";
+        cout<<'\n';
+    }
+    for(ll i=1;i<=node;i++)
+    {
+        if(vis[i]==0)
+        {
+            dfs(i);
+        }
+    }
     if(Cycle)
     {
         cout<<"Cycle detected. Not conflict serializable\n";
