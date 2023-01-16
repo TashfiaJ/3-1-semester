827fb4b76bb51744ccacae051aa239bdba6b17b9
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-01-17 05:57:56 +0600

    Create WordToPDFConverter.java

---
+++ Design Pattern/Factory Method/WordToPDFConverter.java
@@ -0,0 +1,6 @@
+class WordToPDFConverter extends Converter {
+    // implementation details specific to converting Word to PDF
+    public void convert(String file) {
+        // code to convert word to pdf
+    }
+}
