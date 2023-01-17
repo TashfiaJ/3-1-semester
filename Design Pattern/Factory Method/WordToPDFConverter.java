3bfd5b0574f5712429d5afae66507f9a836ed446
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-01-17 09:38:03 +0600

    Delete WordToPDFConverter.java

--- Design Pattern/Factory Method/WordToPDFConverter.java
+++
@@ -1,6 +0,0 @@
-class WordToPDFConverter extends Converter {
-    // implementation details specific to converting Word to PDF
-    public void convert(String file) {
-        // code to convert word to pdf
-    }
-}
