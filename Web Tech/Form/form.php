fd657a8eb3cdeff40d81b151a36bcb7d4d5d1e93
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-02-13 21:24:42 -0800

    Create form.php

---
+++ Web Tech/Form/form.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="insert.php" method="post">
+Name: <input type="text" name="my_name"><br><br>
+Price: <input type="number" name="my_price"><br><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
