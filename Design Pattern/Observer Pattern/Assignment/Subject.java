b5bd4f3281f0989dde4b5b0274b1225d2e98cb79
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-02-28 09:04:17 +0600

    Create Subject.java

---
+++ Design Pattern/Observer Pattern/Assignment/Subject.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Subject {
+    private List<Observer> observers = new ArrayList<>();
+
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void unregisterObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update(this);
+        }
+    }
+}
