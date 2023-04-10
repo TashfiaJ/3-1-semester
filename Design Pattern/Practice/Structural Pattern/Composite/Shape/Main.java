c0be99e5d15af12a984bde857f8bd33178d3106c
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-04-10 21:05:35 +0600

    Create Main.java

---
+++ Design Pattern/Practice/Structural Pattern/Composite/Shape/Main.java
@@ -0,0 +1,14 @@
+package StructuralPattern.Composite;
+
+public class Main {
+    public static void main(String[] args) {
+        Component square = new Square();
+        Component component = new Picture();
+        Component triangle = new Triangle();
+
+        component.add(square);
+        component.add(triangle);
+
+        component.draw();
+    }
+}
