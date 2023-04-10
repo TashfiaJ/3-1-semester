6fd067685c1c138d6de198ace6b79086a7783cbe
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-04-11 02:56:29 +0600

    Create ChessePizza.java

---
+++ Design Pattern/Practice/Creational Pattern/Factory/ChessePizza.java
@@ -0,0 +1,13 @@
+public class ChessePizza implements Pizza {
+    @Override
+    public void pizzaType() {
+        System.out.println("This is a cheese pizza.");
+    }
+
+    @Override
+    public void addElements() {
+        System.out.println("Adding cheese..");
+    }
+
+
+}
