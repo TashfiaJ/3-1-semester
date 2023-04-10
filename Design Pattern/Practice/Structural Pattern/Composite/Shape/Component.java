856e229270030b7ca9bba138ece7c5ad31d738d2
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-04-10 21:14:58 +0600

    Update Component.java

--- Design Pattern/Practice/Structural Pattern/Composite/Shape/Component.java
+++ Design Pattern/Practice/Structural Pattern/Composite/Shape/Component.java
@@ -2,9 +2,5 @@ package StructuralPattern.Composite;
 
 public interface Component {
     void draw();
-    //add
-    void add(Component component);
-    //remove
-    void remove(Component component);
-    Component getChild(int x);
+
 }
