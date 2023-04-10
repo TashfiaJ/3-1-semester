ad84b95e2beb53a4bcca357ab5cd17da483008d5
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-04-11 02:19:22 +0600

    Create PizzaBuilder.java

---
+++ Design Pattern/Practice/Creationsl Pattern/Builder/PizzaBuilder.java
@@ -0,0 +1,6 @@
+public interface PizzaBuilder {
+    public void setPizzaDough(String  pizzaDough);
+    public void setPizzaMeat(String pizzaMeat);
+    public void setPizzaSauce(String pizzaSauce);
+    public void setPizzaToppings(String pizzaToppings);
+}
