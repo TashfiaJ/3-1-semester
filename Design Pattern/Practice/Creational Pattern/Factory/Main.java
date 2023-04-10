dc29baafd7fcd6d86539a3b3cf81fdea39c84112
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-04-11 02:55:26 +0600

    Update Main.java

--- Design Pattern/Practice/Creational Pattern/Factory/Main.java
+++ Design Pattern/Practice/Creational Pattern/Factory/Main.java
@@ -1,1 +1,30 @@
 
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PizzaMaker pizzaMaker;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter pizza name: ");
+        String name = input.nextLine();
+        if (name.equals("Cheese Pizza")) {
+            pizzaMaker = new CheesePizzaMaker();
+        } else if (name.equals("Chicken Pizza")) {
+            pizzaMaker = new ChickenPizzaMaker();
+        } else {
+            pizzaMaker = null;
+        }
+
+        if (pizzaMaker != null) {
+            pizzaMaker.pizzaShop();
+            pizzaMaker.pizzaMake().pizzaType();
+            pizzaMaker.pizzaMake().addElements();
+        } else {
+            System.out.println("No such pizza available.");
+        }
+
+
+    }
+}
