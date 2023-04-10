bf5fb74ebae3fb6f7e7efcfe619224c1bdec2c96
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-04-11 02:17:51 +0600

    Create CheesePizzaBuilder.java

---
+++ Design Pattern/Practice/Creationsl Pattern/Builder/CheesePizzaBuilder.java
@@ -0,0 +1,42 @@
+public class CheesePizzaBuilder implements PizzaBuilder{
+    private String pizzaDough, pizzaMeat, pizzaSauce, pizzaToppings;
+    @Override
+    public void setPizzaDough(String pizzaDough) {
+        this.pizzaDough=pizzaDough;
+    }
+
+    public String getPizzaDough() {
+        return pizzaDough;
+    }
+
+    @Override
+    public void setPizzaMeat(String pizzaMeat) {
+        this.pizzaMeat=pizzaMeat;
+    }
+
+    public String getPizzaMeat() {
+        return pizzaMeat;
+    }
+
+    @Override
+    public void setPizzaSauce(String pizzaSauce) {
+        this.pizzaSauce=pizzaSauce;
+    }
+
+    public String getPizzaSauce() {
+        return pizzaSauce;
+    }
+
+    @Override
+    public void setPizzaToppings(String pizzaToppings) {
+        this.pizzaToppings=pizzaToppings;
+    }
+
+    public String getPizzaToppings() {
+        return pizzaToppings;
+    }
+
+    public CheesePizza getCheesePizza(){
+        return new CheesePizza(pizzaDough,pizzaMeat,pizzaSauce,pizzaToppings);
+    }
+}
