0704ce39a992d7cf97a04068d1a6696c4f8c017b
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-04-11 07:57:06 +0600

    Create Main.java

---
+++ Design Pattern/Practice/Structural Pattern/Facade/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        FacadeGame facadeGameEngine = new FacadeGame();
+
+        facadeGameEngine.carGame();
+        facadeGameEngine.bikeGame();
+    }
+}
