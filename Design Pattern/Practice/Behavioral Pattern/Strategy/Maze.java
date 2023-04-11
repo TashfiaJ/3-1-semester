26d9bea44b9bc6a8ad2555b9151222759a7db40a
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-04-11 07:20:27 +0600

    Create Maze.java

---
+++ Design Pattern/Practice/Behavioral Pattern/Strategy/Maze.java
@@ -0,0 +1,6 @@
+public class Maze implements AttackStrategy{
+    @Override
+    public void attack(String name) {
+        System.out.println("Player has chosen potion and attacked "+name);
+    }
+}
