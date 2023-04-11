1c8d6a2cc272976640853eb1c8941c803b3c6f87
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-04-11 08:02:33 +0600

    Create Main.java

---
+++ Design Pattern/Practice/Behavioral Pattern/Template/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+       Computer laptop = new Laptop();
+       laptop.makeComputer();
+
+       Computer desktop = new Desktop();
+       desktop.makeComputer();
+    }
+}
