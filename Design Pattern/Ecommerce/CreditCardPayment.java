f74d2682c5070f5f9cdff2e8a854b033e7d79f74
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-04-12 06:36:06 +0600

    Create CreditCardPayment.java

---
+++ Design Pattern/Ecommerce/CreditCardPayment.java
@@ -0,0 +1,18 @@
+public class CreditCardPayment implements Payment {
+    private String cardNumber;
+    private String cvv;
+    private String expirationDate;
+
+    public CreditCardPayment(String cardNumber, String cvv, String expirationDate) {
+        this.cardNumber = cardNumber;
+        this.cvv = cvv;
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public boolean pay(double amount) {
+
+        System.out.println("Processing credit card payment of $" + amount + "...");
+        return true;
+    }
+}
