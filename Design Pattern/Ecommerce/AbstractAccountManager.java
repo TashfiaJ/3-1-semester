35cc56aea2274ce0747ab8c5a53421de9f79986d
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-04-12 06:34:25 +0600

    Create AbstractAccountManager.java

---
+++ Design Pattern/Ecommerce/AbstractAccountManager.java
@@ -0,0 +1,21 @@
+public abstract class AbstractAccountManager {
+    protected User user;
+
+    public AbstractAccountManager(User user) {
+        this.user = user;
+    }
+
+    public void displayAccountInfo() {
+        displayName();
+        displayEmail();
+        displayAddress();
+        displayPurchaseHistory();
+        displayEditButton();
+    }
+
+    protected abstract void displayName();
+    protected abstract void displayEmail();
+    protected abstract void displayAddress();
+    protected abstract void displayPurchaseHistory();
+    protected abstract void displayEditButton();
+}
