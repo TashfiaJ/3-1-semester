04af80f6205af0e2696cb4713d5c905c2e33f742
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-04-12 06:36:31 +0600

    Create CryptocurrencyPayment.java

---
+++ Design Pattern/Ecommerce/CryptocurrencyPayment.java
@@ -0,0 +1,12 @@
+public class CryptocurrencyPayment implements Payment {
+    private String walletAddress;
+    private String privateKey;
+
+    // constructor and other methods
+
+    @Override
+    public boolean pay(double amount) {
+        System.out.println("Processing Crypto payment of $" + amount + "...");
+        return true;
+    }
+}
